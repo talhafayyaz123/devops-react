f847464e29a72aed3ccfa58c083c2d8bf5b3f91d
Talha Fayyaz <42764771+talhafayyaz123@users.noreply.github.com>
2024-08-21 22:07:44 +0500

    hh

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import './App.css'
-import Toast from './components/Toast'
 import AllRoutes from './routers/AllRoutes'
 function App() {
 
   return (
     <>
-      <Toast/>
       <AllRoutes/>
     </>
   )
